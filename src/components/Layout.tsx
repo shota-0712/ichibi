8bccef9041eb9f52343967ce23fa3ddd988b7a83
Re-Hatch <system@re-hatch.jp>
2025-07-05 20:47:43 +0900

    ロゴをichigo_ichibi_logo.pngに変更

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
           <div className="flex items-center justify-between py-4 md:py-6">
             <Link to="/" className="flex items-center">
               <img 
-                src="/ichibi_logo_transparent.svg" 
+                src="/ichigo_ichibi_logo.png" 
                 alt="一期一美" 
                 className="w-20 h-20 md:w-24 md:h-24 object-contain drop-shadow-lg"
                 width="96"
